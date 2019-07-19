46796b268d02c1535ac6bcf1cbc416e3cd813ca6
Dean Foster <Deanfoster11@gmail.com>
2019-07-19 16:59:20 -0400

    Add Selection Sort

--- bubble_sort/main.js
+++ bubble_sort/main.js
@@ -2,14 +2,15 @@
 
 var list1 = [345, 6, 2, 56, 2, 87, 8, 6, 7, 60]
 var list2 = [1, 2, 3, 4, 5, 7, 8, 10];
-var list3 = list2.reverse();
+var list3 = list2.slice().reverse();
 
 console.log("Original list: " + list3);
 bubbleSort(list3)
 console.log("Sorted list: " + list3);
 
 /**
- * Bubble Sort (ascending). Simplest sorting algorithm. O(N^2) average case.
+ * Bubble Sort (ascending). Simplest sorting algorithm. O(N^2) average time complexity.
+ * https://www.geeksforgeeks.org/bubble-sort/
  * @param {number[]} list 
  */
 function bubbleSort(list) {
